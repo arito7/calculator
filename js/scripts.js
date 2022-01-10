637fa4bff5b8da12f9631e5009fa950f61f31f0e
Yuji T <accmanager1990@gmail.com>
2022-01-10 16:05:40 +0900

    Add working odin version of calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,41 @@
-const divDisplay = document.querySelector('.display');
 const btnDigit = document.querySelectorAll('.btns > .btn.digit');
 const btnClear = document.querySelector('.btn.clear');
 const btnOps = document.querySelectorAll('.btns > .btn.op');
-let displayValue = '';
-let selectedOperations = [];
-let selectedNumbers = [];
+const divAnswer = document.querySelector('.display > .answer');
+const divEquation = document.querySelector('.display > .equation');
 let currentNumber = '';
+const equation = {
+    number: '',
+    numbers: [],
+    ops: [],
+    answer: '',
+    pushNumber:function(){
+        this.numbers.push(parseFloat(this.number));
+        this.number = '';
+    },
+    pushOp:function(op){
+        this.ops.push(op);
+    },
+    reset:function(){
+        this.number = '';
+        this.numbers = [];
+        this.ops = [];
+        this.answer = '';
+    },
+    getEquation:function(){
+        if(this.ops.length === 0){
+            return this.number;
+        } else {
+            return this.numbers[0] + this.ops[0] + this.number;
+        }
+    },
+}
 const operators = {
-    'plus': '+',
-    'div': '÷',
-    'minus':'-',
-    'mult':'×',
-    'equals': '=',
+    plus: '+',
+    div: '÷',
+    minus:'-',
+    mult:'×',
+    equals: '=',
 }
 
 function add(n1, n2){
@@ -27,7 +51,11 @@ function multiply(n1, n2){
 }
 
 function divide(n1, n2){
-    return n1 / n2;
+    if (n2 != 0){
+        return n1 / n2;
+    } else {
+        return undefined;
+    }
 }
 
 function operate(operation, n1, n2){
@@ -51,123 +79,75 @@ function operate(operation, n1, n2){
 
 
 /**
- * outputs value of the variable displayValue to the calculators divDisplay.
+ * outputs value of the variable equation to the calculators divDisplay.
  */
 function updateDisplay(){
-    divDisplay.textContent = displayValue;
+    divEquation.textContent = equation.getEquation();
+    divAnswer.textContent = equation.answer;
 }
 
-
 /**
- * Event Listeners
- */
-
-document.addEventListener('keydown', e => {
-    console.log(e.code);
-    switch(e.code){
-        case 'Digit0':
-            displayValue += '0';
-            updateDisplay();
-            break;
-        case 'Digit1': 
-            displayValue += '1';
-            updateDisplay();
-            break;
-        case 'Digit2':
-            displayValue += '2';
-            updateDisplay();
-            break;
-        case 'Digit3':
-            displayValue += '3';
-            updateDisplay();
-            break;
-        case 'Digit4':
-            displayValue += '4';
-            updateDisplay();
-            break;
-        case 'Digit5':
-            displayValue += '5';
-            updateDisplay();
-            break;
-        case 'Digit6':
-            displayValue += '6';
-            updateDisplay();
-            break;
-        case 'Digit7':
-            displayValue += '7';
-            updateDisplay();
-            break;
-        case 'Digit8':
-            displayValue += '8';
-            updateDisplay();
-            break;
-        case 'Digit9':
-            displayValue += '9';
-            updateDisplay();
-            break;
-    }
-})
-
-/**
- * update displayValue to be an empty string
+ * CLEAR BUTTON
+ * update equation to be an empty string
  * and call updateDisplay();
  */
 btnClear.addEventListener('click', clearDisplay);
 function clearDisplay(){
-    currentNumber = '';
-    displayValue = '';
-    selectedNumbers = [];
-    selectedOperations = [];
+    equation.reset();
     updateDisplay();
 }
 
 /**
  * DIGIT CLICK
- * When a digit is clicked update value of displayValue variable
+ * When a digit is clicked update value of equation variable
  * and call updateDisplay()
  */
 for (let btn of btnDigit) {
-    btn.addEventListener('click', e =>{
-        currentNumber += btn.textContent;
-        displayValue = currentNumber;
+    btn.addEventListener('click', () => {
+        // if selectedNumbers is empty means this is a new calculation so reset previous answer
+        if(equation.numbers.length === 0){
+            equation.answer = '';
+        }
+        equation.number += btn.textContent;
         updateDisplay();
     });
 }
 
+/**
+ * OPERATOR CLICK
+ */
 for (let btn of btnOps){
     btn.addEventListener('click',operation)
 }
 function operation(e){
-    addNumber(currentNumber);
-    currentNumber = '';
-    if(e.target.textContent !== operators.equals){
-        addOperation(e.target.textContent);
-        calculate();
-        updateDisplay();
-    } else {
-        calculate();
+    // this check is to ignore any operational inputs when the
+    // equation is empty
+    if (equation.number != ''){
+        equation.pushNumber();
+        equation.pushOp(e.target.textContent);
+        if (equation.numbers.length > 1){
+            calculate();
+        }
         updateDisplay();
     }
 }
 
-function addNumber(num){
-    selectedNumbers.push(parseFloat(num));
-}
-
-function addOperation(operation){
-    selectedOperations.push(operation);
-}
-
 function calculate(){
-    if (selectedNumbers.length > 1){
+    if (equation.numbers.length > 1){
         // perform operation on first and second number store result as first number
-        selectedNumbers[0] = operate(selectedOperations[0], selectedNumbers[0], selectedNumbers[1]);
+        equation.numbers[0] = operate(equation.ops[0], equation.numbers[0], equation.numbers[1]);
+        // remove the operation that was just performed
+        equation.ops.splice(0,1);
+        // remove the 2nd number, the first number is now the result of the operation that was just performed
+        equation.numbers.splice(1);
+        equation.answer = equation.numbers[0];
+        // 14 is the max size that can be displayed
+        equation.answer = String(equation.answer).slice(0,14);
         // display first number
-        displayValue = selectedNumbers[0];
+        if (equation.numbers[0] === undefined){
+            equation.answer = 'tsk tsk tsk';
+            equation.numbers = [];
+        }
         updateDisplay();
-        // remove 2nd number that was used in operation
-        selectedNumbers.splice(1,1);
-        // remove the operator
-        selectedOperations.splice(0,1);
     } 
 }
